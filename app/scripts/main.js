a51e905fba391b40288374b6b1337839e9fdc441
Gabriel Nadler <nadler.gabriel@gmail.com>
2015-11-21 13:25:06 +0100

    eve page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,19 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+document.addEventListener('DOMContentLoaded', function() {
+    if (document.location.hash === '') {
+        document.location.hash = 'home';
+    } else {
+        var radioButton = document.querySelector(document.location.hash);
+        if (radioButton !== null) {
+            radioButton.checked = true;
+        } else {
+            document.location.hash = 'home';
+        }
+    }
+    var labels = document.querySelectorAll('label');
+    for (var i = 0; i < labels.length; i++) {
+        var label = labels[i];
+        label.onclick = function(event) {
+            document.location.hash = event.target.getAttribute('for');
+        }
+    }
+});
